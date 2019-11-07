3a88b2e3e98aa52030d047cc3f98c073cce761ee
Nikojoel <niko-holopainen@hotmail.com>
2019-11-07 13:28:43 +0200

    express app, get / and get /demo

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("moi");
+'use strict'
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello from my Node server');
+});
+
+app.get('/demo', (req, res) => {
+	res.send('demo');
+});
+
+app.listen(3000, () => {
+	console.log('server app start?');
+});
